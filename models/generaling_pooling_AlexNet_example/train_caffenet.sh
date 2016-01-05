b69f00cc31581ecc6356fb64ef82a880a6e79fdc
tbrsun <tbrsun@gmail.com>
2016-01-04 18:53:09 -0800

    Update train_caffenet.sh

--- models/generaling_pooling_AlexNet_example/train_caffenet.sh
+++ models/generaling_pooling_AlexNet_example/train_caffenet.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-#GLOG_logtostderr=1 ./../../build/tools/caffe train --solver=solver.prototxt
-GLOG_logtostderr=1 ./../../build/tools/caffe train --solver=solver.prototxt --snapshot=caffe_alexnet_train_iter_440000.solverstate
+GLOG_logtostderr=1 ./../../build/tools/caffe train --solver=solver.prototxt
